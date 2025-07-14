d6a8a10d823b7680888e2e90095a69a29b0b0e17
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-07-14 22:27:06 +0330

    loading has been removed for now

--- src/pages/homePage/MyDayPage.tsx
+++ src/pages/homePage/MyDayPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import ConfirmModal from '../../components/confirmModal/ConfirmModal'
-import LoadingIcon from '../../components/loadingIcon/LoadingIcon'
 import TaskBox from '../../components/taskBox/TaskBox'
 import type { TasksType } from '../../contexts/mainContext/MainContext'
 import { useMyContext } from '../../contexts/mainContext/useMyContext'
@@ -27,21 +26,15 @@ export default function MyDayPage() {
 
     return (
         <div className={styles.king}>
-            {
-                tasks.length > 0 ?
-                    tasks.map(task => (
-                        <TaskBox
-                            key={task.id}
-                            task={task}
-                            tasks={tasks}
-                            onMakeTaskImportant={changeTasksState}
-                            onCheckTask={changeTasksState}
-                        />
-                    ))
-                    :
-                    <LoadingIcon />
-
-            }
+            {tasks.map(task => (
+                <TaskBox
+                    key={task.id}
+                    task={task}
+                    tasks={tasks}
+                    onMakeTaskImportant={changeTasksState}
+                    onCheckTask={changeTasksState}
+                />
+            ))}
             <AddNewTaskBtn />
             {confirmModalInfo.isModalOpen && <ConfirmModal onConfirm={changeTasksState} />}
         </div>
