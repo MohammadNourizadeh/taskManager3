be14cfdde8c7be7d499637e79c7cf0117a436c5e
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-08-04 17:03:01 +0330

    abstrac login and sign up logic

--- back-end/authentication/login.php
+++ back-end/authentication/login.php
@@ -1,1 +1,28 @@
-<php ?>
+<?php
+
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$fetchedData = file_get_contents("php://input");
+$data = json_decode($fetchedData, true);
+
+if (isset($data)) {
+    $db = mysqli_connect('localhost', 'root', '', 'task_manager');
+    $username = $data['username'];
+    $password = $data['password'];
+
+    $userFromDb = mysqli_query($db, "
+        SELECT * FROM `users`
+        WHERE `username` = '$username' AND `password` = '$password'
+    ");
+
+    if (mysqli_fetch_assoc($userFromDb)) {
+        $dataSend = ['error' => false, 'msg' => 'welcome'];
+        echo json_encode($dataSend);
+    } else {
+        $dataSend = ['error' => true, 'msg' => 'user not found'];
+        echo json_encode($dataSend);
+    }
+}
--- back-end/authentication/signup.php
+++ back-end/authentication/signup.php
@@ -1,6 +1,26 @@
-<php 
+<?php
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$getData = file_get_contents("php://input");
+$data = json_decode($getData, true);
 
 
+if (isset($data)) {
+    $db = mysqli_connect('localhost', 'root', '', 'task_manager');
 
+    $email = $data['email'];
+    $username = $data['username'];
+    $password = $data['password'];
 
-?>
+    mysqli_query($db, "
+        INSERT INTO `users`
+        (`email` , `username` , `password`)
+        values ('$email' , '$username' , '$password')
+    ");
+
+    $dataSend = ['error' => false, 'msg' => 'registration was successful'];
+    echo json_encode($dataSend);
+}
