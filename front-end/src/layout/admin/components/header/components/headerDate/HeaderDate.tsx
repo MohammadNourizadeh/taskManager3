c615f2c9f6f7e1f2d2c348141d5c63fbd2760f16
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-08-31 10:54:39 +0330

    header date is ready

--- front-end/src/layout/admin/components/header/components/headerDate/HeaderDate.tsx
+++ front-end/src/layout/admin/components/header/components/headerDate/HeaderDate.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
 import styles from './HeaderDate.module.scss'
 
 export default function HeaderDate() {
+    // state
+    const [date, setDate] = useState<string | null>(null)
+
+    // side effect
+    useEffect(() => {
+        fetch('http://localhost:8080/php/task_manager/getDate.php')
+            .then(res => res.text())
+            .then(data => {
+                const myNewDate = new Date(data)
+                setDate(myNewDate.toLocaleString("en-US", {
+                    day: 'numeric',
+                    weekday: "long",
+                    month: 'long',
+                }))
+
+            })
+    }, [])
+
     return (
-        <div className={styles.date}>Wednesday, June 3</div>
+        <div className={styles.date}>
+            {date}
+        </div>
     )
 }
