57b66f1b13d0a90cae5a0592631e4b48c38b5fea
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-08-31 11:12:56 +0330

    get date backend file for header date

---
+++ back-end/headerDate/getDate.php
@@ -0,0 +1,9 @@
+<?php
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+$date = gmdate('c');
+echo $date;
