9ebe547a3c1e61583444474b70ba3fa694fb08ce
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-09-03 21:32:14 +0330

    working on light and dark mode

--- front-end/src/layout/auth/Auth.tsx
+++ front-end/src/layout/auth/Auth.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import styles from './Auth.module.scss';
+import { useSelector } from 'react-redux';
+import type { RootState } from '../../store/store';
 
 export default function Auth() {
+    // redux
+    const setting = useSelector((state: RootState) => state.setting.setting)
+
     return (
-        <div className={styles.king}>
+        <div className={styles.king} id={setting.mode === 'dark' ? styles.darkMode : styles.lightMode}>
             <Outlet />
         </div>
     )
