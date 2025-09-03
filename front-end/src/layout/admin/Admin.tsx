b977b052156d9d373f562d5e048643af2975bf85
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-09-03 21:39:01 +0330

    working on light and dark mode

--- front-end/src/layout/admin/Admin.tsx
+++ front-end/src/layout/admin/Admin.tsx
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import type { RootState } from '../../store/store'
 import styles from './Admin.module.scss'
 import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 
 export default function Admin() {
+    // redux
+    const setting = useSelector((state: RootState) => state.setting.setting)
+
     return (
-        <div className={styles.king}>
+        <div className={styles.king} id={setting.mode === 'dark' ? styles.darkMode : styles.lightMode}>
             <div className={styles.sidebarPart}>
                 <Sidebar />
             </div>
--- front-end/src/layout/auth/Auth.tsx
+++ front-end/src/layout/auth/Auth.tsx
@@ -1,7 +1,7 @@
-import { Outlet } from 'react-router-dom';
-import styles from './Auth.module.scss';
 import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 import type { RootState } from '../../store/store';
+import styles from './Auth.module.scss';
 
 export default function Auth() {
     // redux
