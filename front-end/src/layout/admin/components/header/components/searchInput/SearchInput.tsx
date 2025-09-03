749814112bd45d9ae2041260cc697331734128b7
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-09-03 21:48:35 +0330

    working on light and dark mode

--- front-end/src/layout/admin/components/header/components/searchInput/SearchInput.tsx
+++ front-end/src/layout/admin/components/header/components/searchInput/SearchInput.tsx
@@ -1,10 +1,15 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../../../../../store/store'
 import styles from './SearchInput.module.scss'
 
 export default function SearchInput() {
+    // redux
+    const setting = useSelector((state: RootState) => state.setting.setting)
+
     return (
-        <div className={styles.inputContainer}>
+        <div className={styles.inputContainer} id={setting.mode === 'dark' ? styles.darkMode : styles.lightMode}>
             <input type="text" placeholder='search' />
             <div className={styles.searchIcon}>
                 <FontAwesomeIcon icon={faSearch} />
@@ -12,4 +17,3 @@ export default function SearchInput() {
         </div>
     )
 }
-  
