07103c7214a170da7e2619fda70918f7a967197e
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-09-13 18:26:40 +0330

    style of setting dropdown changed

--- front-end/src/layout/admin/components/sidebar/components/sidebarItems/components/sidebarSettingItem/SidebarSettingItem.tsx
+++ front-end/src/layout/admin/components/sidebar/components/sidebarItems/components/sidebarSettingItem/SidebarSettingItem.tsx
@@ -1,7 +1,9 @@
+import { faSun } from '@fortawesome/free-regular-svg-icons'
 import { faMoon, type IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState, type ChangeEvent } from 'react'
+import { type ChangeEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import useToggle from '../../../../../../../../customHooks/useToggle/useToggle'
 import { setPartOfSetting } from '../../../../../../../../store/slices/setting'
 import type { RootState } from '../../../../../../../../store/store'
 import styles from './SidebarSettingItem.module.scss'
@@ -12,7 +14,7 @@ export default function SidebarSettingItem({ icon, name }: { icon: IconDefinitio
     const dispatch = useDispatch()
 
     // state
-    const [isDropDownOpen, setIsDropDownOpen] = useState<boolean>(false)
+    const [isDropDownOpen, setIsDropDownOpen] = useToggle()
 
     // func
     const changeSetting = (e: ChangeEvent<HTMLSelectElement>, changedStateName: keyof typeof setting) => {
@@ -33,35 +35,36 @@ export default function SidebarSettingItem({ icon, name }: { icon: IconDefinitio
         })
             .then(res => {
                 if (res.ok) {
-                    dispatch(setPartOfSetting({ settingItem: changedStateName, newSettingValue }))  
+                    dispatch(setPartOfSetting({ settingItem: changedStateName, newSettingValue }))
                 }
             })
     }
 
     return (
-        <li id={setting.theme === 'dark' ? styles.darkMode : styles.lightMode}>
-            <div className={styles.settingItem} onClick={() => { setIsDropDownOpen(prev => !prev) }} id={isDropDownOpen ? styles.chosenItem : undefined}>
+        <li className={`${styles.settingItem} ${isDropDownOpen ? styles.chosenItem : undefined}`} id={setting.theme === 'dark' ? styles.darkMode : styles.lightMode}>
+            <div className={styles.settingItem} onClick={() => { setIsDropDownOpen() }} >
                 <span style={{ color: 'rgb(51, 139, 246)' }}>
                     <FontAwesomeIcon icon={icon} />
                 </span>
                 {name}
             </div>
-            <div className={`${styles.dropDown} ${isDropDownOpen ? styles.open : ''}`}>
-                <div className={styles.settingForm}>
-                    <div className={styles.inputAndLabelContainer}>
-                        <label htmlFor="theme">
-                            <span>
-                                <FontAwesomeIcon icon={faMoon} />
-                            </span>
-                            theme
-                        </label>
-                        <select value={setting.theme} onChange={(e) => { changeSetting(e, 'theme') }}>
-                            <option value="dark">dark</option>
-                            <option value="light">light</option>
-                        </select>
+            {isDropDownOpen &&
+                <div className={styles.dropDown}>
+                    <div className={styles.settingForm}>
+                        <div className={styles.inputAndLabelContainer}>
+                            <label htmlFor="theme">
+                                <span>
+                                    <FontAwesomeIcon icon={setting.theme === 'light' ? faSun : faMoon} />
+                                </span>
+                                theme :
+                            </label>
+                            <select value={setting.theme} onChange={(e) => { changeSetting(e, 'theme') }}>
+                                <option value="dark">dark</option>
+                                <option value="light">light</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </div>}
         </li>
     )
 }
