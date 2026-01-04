df9f5285c9046b9b9f5d2b30b8e85dbdc2a2669a
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2026-01-04 10:40:27 +0330

    date loading is ready

--- front-end/src/layout/admin/components/header/components/headerDate/HeaderDate.tsx
+++ front-end/src/layout/admin/components/header/components/headerDate/HeaderDate.tsx
@@ -1,32 +1,30 @@
-import { useEffect, useState } from 'react'
-import styles from './HeaderDate.module.scss'
-import LoadingIcon from '../../../../../../components/loadingIcon/LoadingIcon'
+import { useEffect, useState } from "react";
+import styles from "./HeaderDate.module.scss";
+import LoadingIcon from "../../../../../../components/loadingIcon/LoadingIcon";
 
 export default function HeaderDate() {
-    // state
-    const [date, setDate] = useState<string | null>(null)
+  // state
+  const [date, setDate] = useState<string | null>(null);
 
-    // side effect
-    useEffect(() => {
-        fetch('http://localhost:8080/php/task_manager/getDate.php')
-            .then(res => res.text())
-            .then(data => {
-                const myNewDate = new Date(data)
-                setDate(myNewDate.toLocaleString("en-US", {
-                    day: 'numeric',
-                    weekday: "long",
-                    month: 'long',
-                }))
+  // side effect
+  useEffect(() => {
+    fetch("http://localhost:8080/php/task_manager/getDate.php")
+      .then((res) => res.text())
+      .then((data) => {
+        const myNewDate = new Date(data);
+        setDate(
+          myNewDate.toLocaleString("en-US", {
+            day: "numeric",
+            weekday: "long",
+            month: "long",
+          })
+        );
+      });
+  }, []);
 
-            })
-    }, [])
-
-    return (
-        <div className={styles.date}>
-            {date !== 'null' ?
-                <LoadingIcon width={24} loadingText={false} />
-                :
-                date}
-        </div>
-    )
+  return (
+    <div className={styles.date}>
+      {date === "null" ? <LoadingIcon width={24} loadingText={false} /> : date}
+    </div>
+  );
 }
